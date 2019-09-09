7d8c393679e2fd7ab8317e410e96eaf0f1071c32
Jyrno Ader <jyrno42@gmail.com>
2019-09-09 14:59:20 +0300

    add my command

--- foo.sh
+++ foo.sh
@@ -1,1 +1,5 @@
-make images DOCKER_REPO=hub.docker.com/jyrno42 DOCKER_TAG=experimental
+make images DOCKER_REPO=jyrno42 DOCKER_TAG=experimental
+
+docker push jyrno42/cert-manager-controller:canary
+docker push jyrno42/cert-manager-webhook:canary
+docker push jyrno42/cert-manager-cainjector:canary
